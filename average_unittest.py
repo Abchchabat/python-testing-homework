c1b4b083604ae33912e66687cce370e33f249d70
Udnikov <venarurvenarud@gmail.com>
2025-06-14 19:45:38 +0300

    Update average_unittest.py

--- average_unittest.py
+++ average_unittest.py
@@ -4,25 +4,29 @@ from average import calculate_average
 
 class TestCalculateAverage(unittest.TestCase):
     def setUp(self):
-        self.tester_name = 'Ivan Petrov'
+        self.tester_name = 'Uydnikov Vladimir'
         print(f'Tested by {self.tester_name}')
-        
+
     def test_positive_numbers(self):
-        result = calculate_average([1.5, 2.5, 3.5])
+        """Проверяем положительное число"""
+        result = calculate_average([1.7, 2.8, 3.9])
         expected_result = 2
         self.assertEqual(result, expected_result)
 
     def test_negative_numbers(self):
-        result = calculate_average([-1.5, -2.5, -3.5])
-        expected_result = -2
+        """Проверяем отрицательные числа"""
+        result = calculate_average([-1.7, -2.8, -3.9])
+        expected_result = -3
         self.assertEqual(result, expected_result)
 
     def test_mixed_numbers(self):
-        result = calculate_average([1.5, -2.5, 3.5])
-        expected_result = 1
+        """Проверяем смешанные положительные и отрицательные числа"""
+        result = calculate_average([1.7, -2.8, 3.9])
+        expected_result = 0
         self.assertEqual(result, expected_result)
 
     def test_empty_list(self):
+        """Проверяем пустой список"""
         result = calculate_average([])
         expected_result = None
         self.assertIsNone(result)

f08c32f176ba07e7f14ed2064f4d9878fd3087dd
Udnikov <venarurvenarud@gmail.com>
2025-06-14 19:44:45 +0300

    Update average_pytest.py

--- average_pytest.py
+++ average_pytest.py
@@ -2,18 +2,21 @@ import pytest
 from average import calculate_average
 
 
-@pytest.fixture(scope="module")
-def tester():
-    return {'tester': 'Ivan Petrov'}
+@pytest.fixture(scope='session', autouse=True)
+def setup_tests():
+    global TESTER_NAME
+    TESTER_NAME = 'Uydnikov Vladimir'
+    print(f'\nTests executed by {TESTER_NAME}\n')
 
 
-@pytest.mark.parametrize("input_data,expected",
+@pytest.mark.parametrize('input_data, expected',
                          [
-                             ([1.5, 2.5, 3.5], 2),
-                             ([-1.5, -2.5, -3.5], -2),
-                             ([1.5, -2.5, 3.5], 1),
-                             ([], None)
-                         ])
-def test_calculate_average(input_data, expected, tester):
-    assert calculate_average(input_data) == expected
-    print(f'{tester["tester"]}: Проверено среднее значение {input_data}, ожидаемое: {expected}.')
+                            ([1.7, 2.8, 3.9], 2),  # Положительные числа
+                            ([-1.7, -2.8, -3.9], -3),  # Отрицательные числа
+                            ([1.7, -2.8, 3.9], 0),  # Смешанный случай
+                            ([], None),  # Пустой список
+                          ]
+                        )
+def test_calculate_average(input_data, expected):
+    actual_result = calculate_average(input_data)
+    assert actual_result == expected, f"Ошибка при проверке набора {input_data}"
